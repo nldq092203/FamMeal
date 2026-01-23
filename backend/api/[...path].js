8e28492dfada9d0aa1814d94c3c11f2be1cd52fe
nldq092203 <lnguye220903@gmail.com>
2026-01-23 16:20:57 +0100

    chore: another try to mep

--- backend/api/[...path].js
+++ backend/api/[...path].js
@@ -12,8 +12,26 @@ let readyPromise;
 
 async function getApp() {
   if (!appPromise) {
+    const fs = require('fs');
+    const path = require('path');
     // eslint-disable-next-line global-require
-    const { buildApp } = require('../dist/app.js');
+    const moduleAlias = require('module-alias');
+
+    const rootDir = path.join(__dirname, '..');
+    const distDir = path.join(rootDir, 'dist');
+    const srcDir = path.join(rootDir, 'src');
+
+    // Register runtime alias so "@/..." works even if build output isn't rewritten.
+    // Prefer dist/ if it exists; otherwise fall back to src/.
+    const aliasTarget = fs.existsSync(distDir) ? distDir : srcDir;
+    moduleAlias.addAlias('@', aliasTarget);
+
+    const distApp = path.join(distDir, 'app.js');
+    const srcApp = path.join(srcDir, 'app.js');
+
+    // Prefer compiled output; fall back to src/app.js if Vercel transpiled TS into src/.
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const { buildApp } = require(fs.existsSync(distApp) ? distApp : srcApp);
     appPromise = buildApp();
   }
 
@@ -36,4 +54,3 @@ module.exports = async function handler(req, res) {
   const app = await getApp();
   app.server.emit('request', req, res);
 };
-
